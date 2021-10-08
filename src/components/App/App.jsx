a7054134732c3d65f450b7e412085cefaabba34a
david <david@MacBook-Pro-david.local>
2021-10-09 02:29:08 +0300

    Revert "Revert "second step done""
    This reverts commit c164f2b83831c423388aca06bf32344f0b55b639.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,46 @@
-import './App.css';
+import React, { Component } from 'react';
 import { NewTaskForm } from '../NewTaskForm/NewTaskForm';
 import { TaskList } from '../TaskList/TaskList';
 import { Footer } from '../Footer/Footer';
 
-const App = () => {
-  const todoData = [
-    {
-      label: 'Completed task',
-      active: false,
-      editing: false,
-      id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
-    },
-    {
-      label: 'Editing task',
-      active: true,
-      editing: true,
-      id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
-    },
-    {
-      label: 'Active task',
-      active: true,
-      editing: false,
-      id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
-    },
-  ];
+import './App.css';
 
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <NewTaskForm />
-      </header>
-      <TaskList todos={todoData} />
-      <Footer />
-    </section>
-  );
-};
+export default class App extends Component {
+  state = {
+    todoData: [
+      {
+        label: 'Completed task',
+        id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
+      },
+      {
+        label: 'Editing task',
+        id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
+      },
+      {
+        label: 'Active task',
+        id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
+      },
+    ],
+  };
 
-export default App;
+  handleDelete = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: todoData.filter((todo) => todo.id !== id),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm />
+        </header>
+        <TaskList todos={this.state.todoData} onDeleted={this.handleDelete} />
+        <Footer />
+      </section>
+    );
+  }
+}
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,31 +1,62 @@
-import './Task.css';
+import { Component } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 
-export const Task = ({ label, active, editing, id }) => {
-  const timeCreated = formatDistanceToNow(
-    new Date(),
-    { includeSeconds: true },
-    { addSuffix: true }
-  );
+import './Task.css';
 
-  let style = active ? 'active' : 'completed';
+export default class Task extends Component {
+  state = {
+    done: false,
+    editing: false,
+  };
 
-  if (editing) {
-    style = 'editing';
+  handleDoneClick = () => {
+    this.setState(({ done }) => {
+      return {
+        done: !done,
+      };
+    });
+  };
+
+  handleEditClick = () => {
+    this.setState(({ editing }) => {
+      return {
+        editing: !editing,
+      };
+    });
+  };
+
+  render() {
+    const { label, id, onDeleted } = this.props;
+    const { done, editing } = this.state;
+
+    const timeCreated = formatDistanceToNow(
+      new Date(),
+      { includeSeconds: true },
+      { addSuffix: true }
+    );
+
+    const className = done ? 'completed' : editing ? 'editing' : 'active';
+
+    return (
+      <li className={className} key={id}>
+        <div className="view">
+          <input
+            className="toggle"
+            type="checkbox"
+            onClick={this.handleDoneClick}
+          />
+          <label>
+            <span className="description">{label}</span>
+            <span className="created">created {timeCreated} ago</span>
+          </label>
+          <button
+            className="icon icon-edit"
+            onClick={this.handleEditClick}
+          ></button>
+          <button className="icon icon-destroy" onClick={onDeleted}></button>
+        </div>
+        <input type="text" className="edit" defaultValue={label} />
+      </li>
+    );
   }
-
-  return (
-    <li className={style} key={id}>
-      <div className="view">
-        <input className="toggle" type="checkbox" />
-        <label>
-          <span className="description">{label}</span>
-          <span className="created">created {timeCreated} ago</span>
-        </label>
-        <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy"></button>
-      </div>
-      <input type="text" className="edit" defaultValue="Editing task" />
-    </li>
-  );
-};
+}
